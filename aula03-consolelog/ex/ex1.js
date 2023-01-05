08509dcce50e98fc011cb9502dbf29d94c91ea50
Thiago <thiagobarbosa3110@gmail.com>
2023-01-04 21:14:03 -0300

    first commit

---
+++ aula03-consolelog/ex/ex1.js
@@ -0,0 +1,6 @@
+console.log(
+  'Meu nome é "Thiago". Estou aprendendo JavaScript às',
+  14 + ':' + 30,
+  'da tarde'
+)
+// Isso vai ser ignorado
---
+++ aula03-consolelog/index.js
@@ -0,0 +1,5 @@
+console.log('Hello world')
+console.log('Teste "desta" coisa')
+console.log(`'aaa' "aaa"`)
+
+console.log(123456, 15.95)
